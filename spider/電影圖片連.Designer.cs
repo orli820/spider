72adca274e891beceef05553000a72990877390d
苡錚 陳 <yikishow@gmail.com>
2022-11-07 21:02:57 +0800

    0

---
+++ spider/電影圖片連.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace spider
+{
+    partial class 電影圖片連
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(619, 46);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(323, 532);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 532);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 94);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "找";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 電影圖片連
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 746);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "電影圖片連";
+            this.Text = "電影圖片連";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ spider/電影圖片連.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spider
+{
+    public partial class 電影圖片連 : Form
+    {
+        public 電影圖片連()
+        {
+            InitializeComponent();
+        }
+        MDAEntities1 db = new MDAEntities1();
+        電影Movies m = new 電影Movies();
+        電影圖片總表MovieImages P = new 電影圖片總表MovieImages();
+        電影圖片MovieIImagesList I = new 電影圖片MovieIImagesList();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var movienem=from a in db.電影Movies 
+                         join b in db.電影圖片總表MovieImages on a.中文標題Title_Cht equals b.電影名稱MovieName
+                         select a.
+        }
+    }
+}
